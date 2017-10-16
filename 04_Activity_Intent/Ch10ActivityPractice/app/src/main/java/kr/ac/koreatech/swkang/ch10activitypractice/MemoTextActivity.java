1efc31aa66201df39cdbd3f1cad5458e3c91e271
Seungwoo Kang <swkang@koreatech.ac.kr>
2017-10-16 21:12:40 +0900

    A template project for building an application with multiple activities

---
+++ 04_Activity_Intent/Ch10ActivityPractice/app/src/main/java/kr/ac/koreatech/swkang/ch10activitypractice/MemoTextActivity.java
@@ -0,0 +1,12 @@
+package kr.ac.koreatech.swkang.ch10activitypractice;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class MemoTextActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_memotext);
+    }
+}
